6412e744a4283c0855aa60adc4047ffa0556d8ce
domivbhh <hariharanvbit123@gmail.com>
2025-01-23 09:54:03 +0530

    feed api done

--- controllers/feedControllers.js
+++ controllers/feedControllers.js
@@ -1,5 +1,6 @@
 const { connection } = require("mongoose")
-const Connection = require("../models/connectionModel")
+const Connection = require("../models/connectionModel");
+const User = require("../models/userModel");
 
 const getConnections=async(req,res,next)=>{
     try {
@@ -26,11 +27,40 @@ const getConnections=async(req,res,next)=>{
     }
 }
 
+const givenRequest = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const pendingRequest = await Connection.find({
+      fromUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("toUserId", [
+      "firstName",
+      "lastName",
+      "gender",
+      "photoUrl",
+      "about",
+    ]);
+
+    // console.log(pendingRequest);
+
+    if (pendingRequest.length > 0) {
+      res.status(200).json({ data: pendingRequest });
+    } else {
+      throw new Error("No request received");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
 
 const pendingRequest=async(req,res)=>{
     try {
         const loggedInUser=req.user    
         const pendingRequest=await Connection.find({toUserId:loggedInUser._id,status:"interested"}).populate('fromUserId',['firstName','lastName','gender','photoUrl','about'])
+
+        // console.log(pendingRequest)
+
         if(pendingRequest.length>0){
             res.status(200).json({data:pendingRequest})
         }
@@ -48,14 +78,47 @@ const pendingRequest=async(req,res)=>{
 const getFeed=async(req,res)=>{
     try {
         const loggedInUser=req.user   
-        const feeds=await Connection.find({}) 
+        const limit=req.query.limit || 2
+        const page=req.query.page || 1
+
+        const skip=(page-1)*limit
+        // console.log(page,limit)
+
+        const connections=await Connection.find({$or:[{fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}]}).select('fromUserId toUserId')
+
+        
+        
+        const hideUser=connections.map((ele)=>ele.fromUserId.toString()===loggedInUser._id.toString()?ele.toUserId:ele.fromUserId)
+        
+
+        const feed=await User.find().select('firstName lastName gender about').skip(skip).limit(limit)
+        
+        const feedUser=feed.filter((ele)=>!hideUser.toString().includes(ele._id.toString()))
+
+        const result=feedUser.filter((ele)=>ele._id.toString()!==loggedInUser._id.toString())
+
+        res.status(200).json({
+                count:result.length,
+                data:result
+        })
+
+        // const hideUserFromFeed=new Set()
+
+        // connections.forEach((ele)=>{
+        //     hideUserFromFeed.add(ele.fromUserId.toString())
+        //     hideUserFromFeed.add(ele.toUserId.toString())
+        // })
+
+        // console.log(hideUserFromFeed)
+
+        
     } 
     catch (error) {
-        
+        res.status(400).json({ message: error.message });   
     }
 }
 
 
 
 
-module.exports={getConnections,pendingRequest,getFeed}
+module.exports={getConnections,pendingRequest,getFeed,givenRequest}
--- routers/feedRouter.js
+++ routers/feedRouter.js
@@ -1,10 +1,10 @@
 const express=require('express')
-const { getConnections, pendingRequest, getFeed } = require('../controllers/feedControllers')
+const { getConnections, pendingRequest, getFeed, givenRequest } = require('../controllers/feedControllers')
 const authenticate = require('../utils/authenticate')
 
 const router=express.Router()
 
-router.get('/connections',authenticate,getConnections).get('/requests',authenticate,pendingRequest).get('/feed',authenticate,getFeed)
+router.get('/connections',authenticate,getConnections).get('/requests',authenticate,pendingRequest).get('/feed',authenticate,getFeed).get('/givenRequest',authenticate,givenRequest)
 
 
 
