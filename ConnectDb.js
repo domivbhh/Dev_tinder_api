a993a6470c1652de38d7e24fb174cdc8e6f92616
domivbhh <hariharanvbit123@gmail.com>
2025-01-26 11:51:16 +0530

    modified

--- ConnectDb.js
+++ ConnectDb.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
 const ConnectDb=async()=>{
-        await mongoose.connect(process.env.MONGO_URI) 
+        await mongoose.connect(
+          "mongodb+srv://hariharanvbit123:vb.123@cluster0.xjbuc.mongodb.net/Dev_tinder?retryWrites=true&w=majority&appName=Cluster0"
+        ); 
 }
 
 module.exports=ConnectDb
